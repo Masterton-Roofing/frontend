3e605531025a4a34602bec5568631c86c718b949
Ben <meowcat@meowcat.site>
2026-02-27 13:32:00 +0000

    success

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,6 @@
+//* Copyright (c) 2026 Masterton Roofing Ltd.
+// All rights reserved.
+// */
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig(

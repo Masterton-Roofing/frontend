eeaa64e641356b3709fbe39be1de8fa59395af77
Ben <meowcat@meowcat.site>
2026-03-03 17:37:59 +0000

    w-full to fit the dropper

--- src/components/dropdownPvcMembrane.jsx
+++ src/components/dropdownPvcMembrane.jsx
@@ -3,7 +3,7 @@ export default function DropdownPvcMembrane() {
     const [open, setOpen] = useState(false);
 
     return (
-        <div className="w-96 mx-auto mt-16">
+        <div className="w-full mx-auto mt-16">
             <button
                 onClick={() => setOpen(!open)}
                 className="w-full flex justify-between items-center px-4 py-3 bg-slate-800 text-white rounded-lg transition"
@@ -19,11 +19,17 @@ export default function DropdownPvcMembrane() {
             </button>
             <div
                 className={`overflow-hidden transition-all duration-300 ${
-                open ? 'max-h-40 opacity-100 mt-2' : 'max-h-0 opacity-0'
+                open ? 'max-h-[1000px] opacity-100 mt-2' : 'max-h-0 opacity-0'
                 }`}
             >
                 <div className="p-4 bg-slate-100 rounded-lg text-slate-700">
-                    text
+                    <span className="text-2xl font-bold mb-2">About</span>
+                    <p className="mb-4">
+                        This PVC roofing membrane is designed to work well on roofs of all shapes and sizes, making it suitable for both simple and more complex designs.
+                        It is made to last, with strong resistance to things like natural effects of UV damage and aging over time.
+                        The material remains flexible, allowing it to move slightly with the building as temperatures change or the structure settles, which helps prevent damage.
+                        When used with mechanically fixed roofing systems, it also helps the roof stay secure by offering excellent resistance to strong winds and uplift forces.
+                    </p>
                 </div>
             </div>
         </div>

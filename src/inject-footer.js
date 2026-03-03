9625b59a19a34e731e92d395418218ecb59423a6
Ben <meowcat@meowcat.site>
2026-03-03 17:07:53 +0000

    shift to react

--- src/inject-footer.js
+++
@@ -1,9 +0,0 @@
-//* Copyright (c) 2026 Masterton Roofing Ltd.
-// All rights reserved.
-// */
-
-fetch('/components/footer.html')
-    .then(response => response.text())
-    .then(data => {
-        document.getElementById("footer").innerHTML = data;
-    });

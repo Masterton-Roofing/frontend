b3f6ab02d4232eb63d22eda3be788f0847706d70
Ben House <165931265+meowmeow10@users.noreply.github.com>
2026-03-05 11:55:40 +0000

    update word

--- src/pages/solutions/pvc.jsx
+++ src/pages/solutions/pvc.jsx
@@ -21,7 +21,7 @@ function Pvc() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           
           <h2 className="text-3xl font-bold mb-8 text-gray-900 text-center md:text-left">
-            Our PVC Solutions
+            Our PVC Roofing Solutions
           </h2>
           
           {/* 

412320a29029c992a7d9a5520da1a40dcfebc804
Ben House <165931265+meowmeow10@users.noreply.github.com>
2026-03-06 11:30:40 +0000

    add tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+    theme: {
+      extend: {
+        colors: {
+          'mr-blue': '#01597c',
+        },
+      },
+    },
+  }
+  // Usage: class="bg-brand-blue"
+  

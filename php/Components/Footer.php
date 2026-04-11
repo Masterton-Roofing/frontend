9607eee603d8d4d4d743534c757cfd89a8b5895e
Ben <meowcat@meowcat.site>
2026-04-11 11:24:02 +0100

    wip: hover colours

--- php/Components/Footer.php
+++ php/Components/Footer.php
@@ -45,10 +45,10 @@ function renderFooter() {
                 Services
               </p>
               <ul class="mt-2 space-y-2">
-                <li><a href="/solutions" class="transition-colors duration-300 text-gray-300 hover:text-teal-400">Residential</a></li>
-                <li><a href="/solutions" class="transition-colors duration-300 text-gray-300 hover:text-teal-400">Commercial</a></li>
-                <li><a href="/solutions" class="transition-colors duration-300 text-gray-300 hover:text-teal-400">Repairs</a></li>
-                <li><a href="/solutions" class="transition-colors duration-300 text-gray-300 hover:text-teal-400">Maintenance</a></li>
+                <li><a href="/solutions" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Residential</a></li>
+                <li><a href="/solutions" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Commercial</a></li>
+                <li><a href="/solutions" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Repairs</a></li>
+                <li><a href="/solutions" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Maintenance</a></li>
               </ul>
             </div>
             <div>
@@ -56,10 +56,10 @@ function renderFooter() {
                 Company
               </p>
               <ul class="mt-2 space-y-2">
-                <li><a href="/about" class="transition-colors duration-300 text-gray-300 hover:text-teal-400">About Us</a></li>
-                <li><a href="/about" class="transition-colors duration-300 text-gray-300 hover:text-teal-400">Team</a></li>
-                <li><a href="/about" class="transition-colors duration-300 text-gray-300 hover:text-teal-400">Careers</a></li>
-                <li><a href="/about" class="transition-colors duration-300 text-gray-300 hover:text-teal-400">Safety</a></li>
+                <li><a href="/about" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">About Us</a></li>
+                <li><a href="/about" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Team</a></li>
+                <li><a href="/about" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Careers</a></li>
+                <li><a href="/about" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Safety</a></li>
               </ul>
             </div>
             <div>

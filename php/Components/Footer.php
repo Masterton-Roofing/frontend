e4e98771762ce337624f8ad3efc0b254abe1cc9c
Ben <meowcat@meowcat.site>
2026-06-13 09:07:50 +0100

    remove footer links

--- php/Components/Footer.php
+++ php/Components/Footer.php
@@ -48,7 +48,7 @@ function renderFooter() {
                 <li><a href="/solutions" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Residential</a></li>
                 <li><a href="/solutions" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Commercial</a></li>
                 <li><a href="/solutions" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Repairs</a></li>
-                <li><a href="/solutions" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Maintenance</a></li>
+                <li><a href="/solutions/drone" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Drone Survey</a></li>
               </ul>
             </div>
             <div>
@@ -58,7 +58,6 @@ function renderFooter() {
               <ul class="mt-2 space-y-2">
                 <li><a href="/about" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">About Us</a></li>
                 <li><a href="/about" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Team</a></li>
-                <li><a href="/about" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Careers</a></li>
                 <li><a href="/about" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Safety</a></li>
               </ul>
             </div>
@@ -69,7 +68,6 @@ function renderFooter() {
               <ul class="mt-2 space-y-2">
                 <li><a href="/projects" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Recent Work</a></li>
                 <li><a href="/projects" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Case Studies</a></li>
-                <li><a href="/projects" class="transition-colors duration-300 text-gray-300 hover:text-[#f2e599]">Testimonials</a></li>
               </ul>
             </div>
             <div>

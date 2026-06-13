f02cfb6a82a83ca22f2b1fc026902da0de59e704
Ben <meowcat@meowcat.site>
2026-06-13 09:31:59 +0100

    update drone about parameter

--- solutions/drone.php
+++ solutions/drone.php
@@ -10,7 +10,7 @@ renderSolutionPageBase([
         [
             'title' => "Drone Surveys",
             'blurb' => "High-tech aerial inspections for precise roof assessment without the need for scaffolding.",
-            'about' => "Our drone surveys provide a safe and cost-effective way to inspect roofs of any height or complexity. Using high-resolution cameras, we can identify leaks, structural damage, and debris without setting foot on the roof.",
+            'about' => "Our drone surveys provide a safe and cost-effective way to inspect roofs of any height or complexity. Using high-resolution cameras can complete video footage with zoomed in photos which can identify potential leaks, damage, and debris without setting foot on the roof.",
             'images' => []
         ]
     ]

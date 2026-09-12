e22ab0281bc4bb3fd2228ea7ffdb90a08abd9197
Ben <meowcat@meowcat.site>
2026-09-12 09:46:57 +0100

    fix drone banner

--- solutions/drone.php
+++ solutions/drone.php
@@ -3,7 +3,7 @@ require_once __DIR__ . '/../php/Layout/SolutionPageBase.php';
 
 renderSolutionPageBase([
     'pageTitle' => "Drone Surveys - Masterton Roofing",
-    'heroImage' => "/public/img/gallery/APPLE 1.jpg",
+    'heroImage' => "/public/img/gallery/Horsham.jpg",
     'heroTitle' => "Drone Footage - Solutions",
     'sectionTitle' => "Drone Surveys",
     'articles' => [

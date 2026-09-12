83a22c59d971a89a7b5572bf1349f424f3de7e0f
Ben <meowcat@meowcat.site>
2026-09-12 09:42:33 +0100

    replace drone video

--- solutions/drone.php
+++ solutions/drone.php
@@ -12,7 +12,7 @@ renderSolutionPageBase([
             'blurb' => "High-tech aerial inspections for precise roof assessment without the need for scaffolding.",
             'about' => "Our drone surveys provide a safe and cost-effective way to inspect roofs of any height or complexity. Using high-resolution cameras can complete video footage with zoomed in photos which can identify potential leaks, damage, and debris without setting foot on the roof.",
             'images' => [],
-            'video' => '/public/img/test.mp4',
+            'video' => '/public/img/vid/portsdown.mp4',
             'videoPoster' => '/public/img/hero.jpg'
         ]
     ]

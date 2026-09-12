5706e2658b36be72ce552aa5370dc4fbf741ab75
Ben <meowcat@meowcat.site>
2026-09-12 11:11:30 +0100

    fix banner image

--- contact.php
+++ contact.php
@@ -15,7 +15,7 @@ renderHeader("Contact Us - Masterton Roofing");
 
 </head>
 
-<section class="hero h-[80vh] md:h-screen bg-cover bg-center" style="background-image: url('/public/img/gallery/grosvenor main.jpg')">
+<section class="hero h-[80vh] md:h-screen bg-cover bg-center" style="background-image: url('/public/img/gallery/Appleslade.jpg')">
     <div class="flex items-center justify-center h-full bg-black/30 px-4">
         <h1 class="header text-4xl md:text-5xl lg:text-6xl text-center text-white font-bold">Contact Us</h1>
     </div>
